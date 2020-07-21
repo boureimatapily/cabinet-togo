626441aa6e9513ed4e638f0d5f49945eeec15ed7
unknown <boureimatapily1@gmail.com>
2020-07-21 02:34:45 +0000

    MOD Add images

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,55 +1,62 @@
 import React from "react";
-import { Container, Grid, Paper, Button} from "@material-ui/core";
+import { Container, Grid, Paper, Button } from "@material-ui/core";
 import { withStyles } from "@material-ui/styles";
 
 // Making styles
 const styles = {
   root: {
-    display:"flex",
-    flexDirection:"column",
-    justifyContent:"space-between",
-   },
-   note:{
-     marginTop:50
-   },
-   firstContainer:{
-     display:"flex",
-     flexDirection:"row",
-     justifyContent:"space-evenly",
-    marginTop:10,
-   },
-   firstContainerFirstDiv:{
-     
-   }
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  note: {
+    marginTop: 50,
+  },
+  firstContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 10,
+  },
+  firstContainerFirstDiv: {
+    width: 400,
+  },
+  getStartButton:{
+    color: "white", 
+    backgroundColor: "#2bd69a", 
+    width:200
+  },
 };
 
-
 class Home extends React.Component {
-  
-  render(){
+  render() {
     const { classes } = this.props;
- 
-  return (
-    <Container fixed>
-      <Grid container style={{ marginTop: 10}} className={classes.root}>
-        <Grid item xs={12} md={12} lg={12}>
-                      {/* first Container  */}
-        <Paper elevation={1} className={classes.firstContainer}>  
-          <div className={classes.firstContainerFirstDiv} >
-            <h2>  Fight COVID-19 With Codetrain and Covid Care Platform </h2>
-            <h4>  Voluptate fugiat reprehenderit sint aliquip  </h4>
-            <Button style={{color:"white", backgroundColor:"#2bd69a"}} > Get Start</Button>
-          </div>
 
-          <div  >
-            <h4>  All about coding </h4>
-          </div>
-          </Paper>
+    return (
+      <Container fixed>
+        <Grid container style={{ marginTop: 10 }} className={classes.root}>
+          <Grid item xs={12} md={12} lg={12}>
+            {/* first Container  */}
+            <Paper elevation={1} className={classes.firstContainer}>
+              <div className={classes.firstContainerFirstDiv}>
+                <h2> Fight COVID-19 With Codetrain and Covid Care Platform </h2>
+                <h4> Voluptate fugiat reprehenderit sint aliquip </h4>
+                <Button 
+                    className={classes.getStartButton}
+                    >
+                  Get Start
+                </Button>
+              </div>
+
+              <div>
+                <h4> All about coding </h4>
+              </div>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
-  );
-}
+      </Container>
+    );
+  }
 }
 
-export default withStyles(styles)(Home)
+export default withStyles(styles)(Home);
