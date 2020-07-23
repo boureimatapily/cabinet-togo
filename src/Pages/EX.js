32557c8c4b6b8ebba1be0704e565e0bdfb694380
unknown <boureimatapily1@gmail.com>
2020-07-23 02:09:46 +0000

    MOD Content

--- src/Pages/EX.js
+++ src/Pages/EX.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 import React, { Component } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
--- src/Pages/Health.js
+++ src/Pages/Health.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import { Button, Grid, Container } from "@material-ui/core";
+import { Grid, Container } from "@material-ui/core";
 // import { Link } from "react-router-dom";
 import health from "../Images/health1.jpg";
-import hospital3 from "../Images/hospital.jpg";
-
+import DoctorLoginModal from "../Components/Login/DoctorLoginModal";
+import DoctorSignupModal from "../Components/Signup/DoctorSignupModal";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,12 +20,18 @@ const useStyles = makeStyles((theme) => ({
     margin: 10,
     marginBottom: 20,
   },
+  healthDoctorsMainSection: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    margin: 10,
+    marginBottom: 20,
+  },
   descriptionSize: {
     width: 300,
   },
-  descriptionSizeTwo: {
-    width: 400,
-  },
+
   getStartButton: {
     color: "white",
     backgroundColor: "#2bd69a",
@@ -33,58 +39,52 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: "bolder",
     marginBottom: 20,
   },
-
 }));
 
-
-
-export default function Hospital(props) {
+export default function Health() {
   const classes = useStyles();
- 
 
   return (
     <Container fixed>
       <Grid container className={classes.root}>
-        <Grid item xs={6} md={6} lg={6}>
-          <Paper className={classes.healthDoctors} elevation={3}>
-            <h3>For Hospitals </h3>
-            <img
-              src={hospital3}
-              style={{ width: 400, heigth: 300 }}
-              alt="hospital"
-            />
-            <h3 className={classes.descriptionSize}>
-              Create a folder for your Patient, Manage your Patient health and
-              give a realtime update to your Doctor
-            </h3>
-            <Button className={classes.getStartButton} >
-              Get Start
-            </Button>
-          </Paper>
-        </Grid>
-
-        <Grid item xs={6} md={6} lg={6}>
-          <Paper className={classes.healthDoctors} elevation={3}>
-            <h3 style={{ marginBottom: 25 }}>
-              Are you Doctors or health professionals
-            </h3>
+        <Grid item xs={12} md={12} lg={12}>
+          <Paper className={classes.healthDoctorsMainSection} elevation={0}>
+            <div>
+               
             <img
               src={health}
               style={{ width: 400, height: 300 }}
               alt="health"
             />
-            <h3 className={classes.descriptionSizeTwo}>
+            </div>
+              <div>
+              <h3 style={{ marginBottom: 25 }}>
+              Are you Doctors or health professionals
+            </h3>
+            <h3 className={classes.descriptionSize}>
               Manage your dialy activities form home, get realtime update about
               hospital, patient health and communicate with ohter Doctors and
               health professionals
             </h3>
-            <Button className={classes.getStartButton}>
-              Get Start
-            </Button>
+              </div>
+
+          </Paper>
+        </Grid>
+      </Grid>
+
+      <Grid container className={classes.root}>
+        <Grid item xs={6} md={6} lg={6}>
+          <Paper className={classes.healthDoctors} elevation={3}>
+            <h2>Log In to Covid Care</h2>
+            <DoctorLoginModal />
           </Paper>
         </Grid>
 
-        
+        <Grid item xs={6} md={6} lg={6}>
+          <Paper className={classes.healthDoctors} elevation={3}>
+            <DoctorSignupModal />
+          </Paper>
+        </Grid>
       </Grid>
     </Container>
   );
