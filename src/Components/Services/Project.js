8ef10066308868485705b2dad2584282770e6cde
unknown <boureimatapily1@gmail.com>
2020-07-25 03:25:34 +0000

    ADD Project.js component

--- src/Components/Services/Project.js
+++ src/Components/Services/Project.js
@@ -1,31 +1,115 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Container } from "@material-ui/core";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import { Container, Grid, Paper, Hidden } from "@material-ui/core";
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+      style={{ width: "100%" }}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
+  };
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: "flex",
+    height: "100%",
+    marginTop: 30,
   },
-  
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
 }));
 
-export default function Project() {
+export default function Projecct() {
   const classes = useStyles();
- 
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-      <Container fixed>
-          <Grid container className={classes.root}>
-              <Grid item xs={12} sm={10} md={12} lg={12}>
-                <h3>
-                La tenue comptable ou tenue de la comptabilité consiste à enregistrer 
-                toutes les pièces comptables (factures d'achat ou de vente, reçus, quittances, notes de frais...)
-                 de l'entreprise, suivant la norme du plan comptable.
-                </h3>
-              </Grid>
+    <Container fixed>
+      <Grid container>
+        <Grid xs={12} sm={12} md={8} lg={8} className={classes.rootContainer}>
+          <h1>Project studies</h1>
+          <div className={classes.root}>
+            <Tabs
+              orientation="vertical"
+              variant="scrollable"
+              value={value}
+              onChange={handleChange}
+              aria-label="Vertical tabs example"
+              className={classes.tabs}
+            >
+              <Tab
+                label="USAGE"
+                {...a11yProps(0)}
+                style={{ fontWeight: "bolder" }}
+              />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+              <Paper style={{ fontWeight: "bolder" }}>
+                To specify by plans, sections and elevations, the forms of the
+                various elements of the construction, the nature and
+                characteristics of the materials and the conditions of their
+                implementation; To determine the location, and the size of all
+                structural elements and all technical equipment; To specify the
+                routes of the supply and evacuation of all fluids; Establish a
+                provisional cost of the work broken down by body of state, on
+                the basis of a pre-quantity survey; To allow the client, in the
+                light of this assessment, to set the estimated cost of carrying
+                out the work and, moreover, to estimate the costs of its
+                operation; To determine the overall deadline for completion of
+                the work, and to conduct a market study.
+              </Paper>
+            </TabPanel>
+          </div>
+        </Grid>
+        <Hidden smDown>
+          <Grid xs={0} sm={0} md={4} lg={4}>
+            <img
+              src={require("../../Images/pen.png")}
+              alt="accountimg"
+              style={{ width: 300, height: 200 }}
+            />
           </Grid>
-      </Container>
-    
+        </Hidden>
+      </Grid>
+    </Container>
   );
 }
